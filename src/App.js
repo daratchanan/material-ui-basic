6bfc43227c790fee478369009e98a1323edccfcc
jarabeenoi <nongnat61@gmail.com>
2021-02-06 20:52:05 +0700

    couter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const [result, setResult] = useState(0);
+   const [isZero, setIsZero] = useState(false);
+
+   const addR = () => {
+      let count = result + 1
+      setResult(count);
+      fan(count);
+   };
+
+   const desR = () => {
+      let count = result - 1
+      setResult(count);
+      fan(count);
+   };
+
+   const resetR = () => {
+      setResult(0);
+   };
+
+   const fan = (v) => {
+      if (v <= 0) {
+         setIsZero(true)
+      } else {
+         setIsZero(false)
+      }
+   };
+
+   return (
+      <>
+         <div style={{ textAlign: "center" }}>
+            <Button
+               variant="contained"
+               color="secondary"
+               onClick={desR}
+               // disabled={result <= 0? true : false}
+               disabled={isZero}
+
+            >
+               -
+            </Button>
+            <TextField
+               id="outlined-basic"
+               variant="outlined"
+               value={result}
+            />
+            <Button variant="contained" color="secondary" onClick={addR}>
+               +
+            </Button>
+            <Button variant="contained" color="secondary" onClick={resetR}>
+               Reset
+            </Button>
+         </div>
+      </>
+   );
 }
 
 export default App;
