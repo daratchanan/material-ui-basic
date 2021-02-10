d444c73581d287fe6b841baf0d911b78970ae6ee
jarabeenoi <nongnat61@gmail.com>
2021-02-10 16:36:16 +0700

    button, styling the active tab

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Header from "../components/ui/Header";
 import theme from "./ui/Theme";
 import Test from "./ui/Test";
@@ -7,12 +8,23 @@ import Test from "./ui/Test";
 function App() {
 
    return (
-      <ThemeProvider theme={theme}> 
-         <Header />
-         Hello
-         {/*  <Test /> */} 
+      <ThemeProvider theme={theme}>
+         <BrowserRouter>
+            <Header />
+            <Switch>
+               <Route exact path="/" component={() => <div>Home</div>} />
+               <Route exact path="/services" component={() => <div>Services</div>} />
+               <Route exact path="/customsoftware" component={() => <div>Custom Software</div>} />
+               <Route exact path="/mobileapps" component={() => <div>Mobile Apps</div>} />
+               <Route exact path="/websites" component={() => <div>Websites</div>} />
+               <Route exact path="/revolution" component={() => <div>The Revolution</div>} />
+               <Route exact path="/about" component={() => <div>About</div>} />
+               <Route exact path="/contact" component={() => <div>Contact</div>} />
+               <Route exact path="/estimate" component={() => <div>Estimate</div>} />
+            </Switch>
+         </BrowserRouter>
       </ThemeProvider>
-   ); 
+   );
 }
 
 export default App;
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -22,7 +22,13 @@ export default createMuiTheme({
          textTransform: "none",
          fontWeight: 700,
          fontSize: "1rem",
-      }
+      },
+      estimate: {
+         fontFamily: "Pacifico",
+         fontSize: "1rem",
+         textTransform: "none",
+         color: "white"
+      },
    }
 });
 
